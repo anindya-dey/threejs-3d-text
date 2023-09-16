992b4848026f75b9211892518815df78054bf657
Anindya Dey <anindya.dey@vista.com>
2023-09-16 18:04:32 +0530

    add 3d text

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import './style.css';
-import { Scene, WebGLRenderer, PerspectiveCamera, Clock } from 'three';
+import {
+  Scene,
+  WebGLRenderer,
+  PerspectiveCamera,
+  Clock,
+  TextureLoader,
+  MeshMatcapMaterial,
+  Mesh,
+  TorusGeometry
+} from 'three';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import { OrbitControls } from 'three/addons/controls/OrbitControls';
@@ -18,19 +27,61 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Camera
 const camera = new PerspectiveCamera(55, size.width / size.height, 0.1, 100);
-camera.position.set(0, 0, 5)
+camera.position.set(-2, 1, 3)
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
+// Textures
+const textureLoader = new TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+
 // 3D Text
 const fontLoader = new FontLoader();
 fontLoader.load(
-  '/textures/fonts/helvetiker_regular.typeface.json',
+  '/fonts/helvetiker_regular.typeface.json',
   (font) => {
-    console.log(font)
+    // Material
+    const material = new MeshMatcapMaterial({ matcap: matcapTexture });
+
+    // Geometry
+    const textGeometry = new TextGeometry(
+      'Anindya Dey',
+      {
+        font,
+        size: 0.5,
+        height: 0.2,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelOffset: 0,
+        bevelSegments: 5
+      }
+    );
+    textGeometry.center();
+
+    const text = new Mesh(textGeometry, material);
+    scene.add(text);
+
+    const donutGeometry = new TorusGeometry(0.3, 0.2, 32, 64);
+
+    for(let i = 0; i < 300; i++) {
+      const donut = new Mesh(donutGeometry, material);
+
+      donut.position.x = (Math.random() - 0.5) * 30;
+      donut.position.y = (Math.random() - 0.5) * 30;
+      donut.position.z = (Math.random() - 0.5) * 30;
+
+      donut.rotation.x = Math.random() * Math.PI
+      donut.rotation.y = Math.random() * Math.PI
+
+      donut.scale.x = donut.scale.y = donut.scale.z = Math.random()
+
+      scene.add(donut);
+    }
   }
 )
 
